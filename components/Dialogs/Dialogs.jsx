1c1d5ad0eccfd7c5e4043fae81133a76a91b09cf
Yaroslav Myratov <yaroslav.muratov.97@inbox.ru>
2022-12-15 16:42:50 +0300

    2

--- components/Dialogs/Dialogs.jsx
+++ components/Dialogs/Dialogs.jsx
@@ -11,20 +11,30 @@ const Message = (props) => {
 
 const Dialogs = () => {
 	
+let dialogsData = [
+	{id:1, name:'Mariel'},
+	{id:2, name:'Mavi'},
+	{id:3, name:'Maria'}
+]
+let messagesData = [
+	{id:1, messages:'Heloo'},
+	{id:2, messages:'How are you'},
+	{id:3, messages:'Salut'}
+]
   return (
 	<div className={s.wrapper}>
 	<div className={s.items}>
-	<DialogItem name="Mariel" id='1'/>
-	<DialogItem name="Mavi" id='2'/>
-	<DialogItem name="Maria" id='3'/>
+	<DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+	<DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+	<DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
 
 	
 	
 	</div>
 	<div className={s.messages}>
-	<Message message="Heloo"/>
-	<Message message="How are you"/>
-	<Message message="Salut"/>
+	<Message message={messagesData[0].messages}/>
+	<Message message={messagesData[1].messages}/>
+	<Message  message={messagesData[2].messages}/>
 
 	</div>
  </div>
--- components/Profile/MyPost/MyPosts.jsx
+++ components/Profile/MyPost/MyPosts.jsx
@@ -1,18 +1,29 @@
 import s from "./MyPosts.module.css";
-import Post from "./Post/Post"
+import Post from "./Post/Post";
 const MyPosts = () => {
-  return <div className={s.content}>
-      
+
+	let postData = [
+		{id:1, messages:'Hi,how are you',likesCount:0},
+		{id:2, messages:'It\'s my first post',likesCount:23},
+		
+	]
+
+
+  return (
+    <div className={s.content}>
       <div>My post</div>
       <div>New post</div>
-		<textarea name="" id="" cols="40" rows="0"></textarea>
-		<button>Add post</button>
-		<button>Remove</button>
-		<div className={s.posts}>
-<Post message='Hi,how are you'likesCount='0'/>
-<Post message="It's my first post" likesCount='23'/>
-<Post />
+      <div>
+        <textarea name="" id="" cols="40" rows="0"></textarea>
+      </div>
+      <div><button>Add post</button>
+      <button>Remove</button></div>
+      <div className={s.posts}>
+        <Post message={postData[0].messages} likesCount={postData[0].likesCount} />
+        <Post message={postData[1].messages } likesCount={postData[1].likesCount} />
+        <Post />
+      </div>
     </div>
-	 </div>
+  );
 };
 export default MyPosts;
